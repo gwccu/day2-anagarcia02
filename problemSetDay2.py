791a505f6e73d03c12f12150dd61d31b4e9bac63
anagarcia02 <annertimes@gmail.com>
2019-02-16 17:30:38 +0000

    Hiii :0

--- problemSetDay2.py
+++ problemSetDay2.py
@@ -1,1 +1,22 @@
 # file name: problemSetDay2.py
+
+userName = input("What's your name?")
+userBirthday = input("What's your birthday?")
+userGrade = input("What grade are you in?")
+userSchool = input("What school do you go to?")
+userFavSubject = input("What's your favorite subject?")
+userGoal = input("what do you want to do when you grow up?")
+
+print(userName)
+print(userBirthday)
+print(userGrade)
+print(userSchool)
+print(userFavSubject)
+print(userGoal)
+
+a = int(input("What is the value of a?"))
+b = int(input("What is the value of b?"))
+c = int(input("What is the value of c?"))
+
+print("x = " (-b - sqrt(b ** 2 - 4 * a * c)) / 2 * a)
+print("x = " (-b + sqrt(b ** 2 - 4 * a * c)) / 2 * a)
